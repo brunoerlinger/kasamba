24e4c550b526f178809cd297f935d58e3db2321e
Leticia Rafael <leticiavanelli@gmail.com>
2017-04-26 12:10:35 -0300

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-=======
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
->>>>>>> master
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :my_products
 
 end

d7846c43f71ade6e0e889a992e8be55ba577139b
Bruno Erlinger <brunoerlinger@gmail.com>
2017-04-26 12:26:15 -0300

    a finalizar products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :name, :description, :price, :address ,presence: :true
+  validates :name, :description, :price, :address , presence: :true
 
 end

d0625af524b7ee06192f345c72400db3218a2e9d
Bruno Erlinger <brunoerlinger@gmail.com>
2017-04-27 11:44:35 -0300

    apenas schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426191344) do
-
+ActiveRecord::Schema.define(version: 20170426180203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +35,6 @@ ActiveRecord::Schema.define(version: 20170426191344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
   create_table "chats", force: :cascade do |t|

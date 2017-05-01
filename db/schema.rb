d206565ec50e14ce3e8097b90f931815d27341e1
Leticia Rafael <leticiavanelli@gmail.com>
2017-05-01 08:56:45 -0300

    friendly id gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170427211729) do
+ActiveRecord::Schema.define(version: 20170428161835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,9 @@ ActiveRecord::Schema.define(version: 20170427211729) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "ancestry"
+    t.string   "slug"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
+    t.index ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
   end
 
   create_table "chats", force: :cascade do |t|

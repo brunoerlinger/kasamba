ea2b5f37b35a6254357203d7e408b947c6226fd8
Leticia Rafael <leticiavanelli@gmail.com>
2017-05-04 12:07:13 -0300

    nao esta crashando! AMEM DEUS

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def show
     @order_itens = current_order.order_itens
-    # @category = Category.find(params[:category])
+    @products = Product.order(view: :desc).first(3)
   end
 
 end
